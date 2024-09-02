c2ac895e5011a9ab557d8cba11c239cc05a53703
Mayur Khodre <mayurkhodre2000@gmail.com>
2024-09-03 01:15:13 +0530

    MODIFY::MAYUR:ADDED VALIDATION ON HOME AND PROFILE PAGE ICON

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { logout } from '../utils/Auth';
 import Tooltip from '@mui/material/Tooltip';
 import { HomeIcon, UserIcon, ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
 
 const Header = () => {
+	const location = useLocation();
+
 	return (
 		<header className="bg-blue-600 text-white shadow-md">
 			<div className="container mx-auto flex justify-between items-center py-4 px-6">
@@ -13,21 +15,25 @@ const Header = () => {
 					Splitwise
 				</Link>
 
-				{/* Navigation Links */}
+				{/* Navigation Icons */}
 				<nav className="flex items-center space-x-6">
 					{/* Home Icon with Tooltip */}
-					<Tooltip title="Home" placement="bottom">
-						<Link to="/home">
-							<HomeIcon className="h-6 w-6 text-white hover:text-gray-300 transition duration-200" />
-						</Link>
-					</Tooltip>
+					{location.pathname !== '/home' && (
+						<Tooltip title="Home" placement="bottom">
+							<Link to="/home">
+								<HomeIcon className="h-6 w-6 text-white hover:text-gray-300 transition duration-200" />
+							</Link>
+						</Tooltip>
+					)}
 
 					{/* Profile Icon with Tooltip */}
-					<Tooltip title="Profile" placement="bottom">
-						<Link to="/profile">
-							<UserIcon className="h-6 w-6 text-white hover:text-gray-300 transition duration-200" />
-						</Link>
-					</Tooltip>
+					{location.pathname !== '/profile' && (
+						<Tooltip title="Profile" placement="bottom">
+							<Link to="/profile">
+								<UserIcon className="h-6 w-6 text-white hover:text-gray-300 transition duration-200" />
+							</Link>
+						</Tooltip>
+					)}
 
 					{/* Logout Icon with Tooltip */}
 					<Tooltip title="Logout" placement="bottom">
